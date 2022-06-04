243fbeaf7686d9fc523370f866fc56aa65ed403e
vincemarcus <vincemarcus1209@gmail.com>
2022-06-04 16:30:36 +0700

    Update DetailScreen's ChapterList component UI.

--- lib/view/details_screen/components/chapter_list.dart
+++ lib/view/details_screen/components/chapter_list.dart
@@ -5,79 +5,85 @@ import '../../../models/products.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-final _db =FirebaseDatabase.instance.reference();
+final _db = FirebaseDatabase.instance.reference();
 
-
-int count=0;
-int numChapter=15;
-
-
-
-
+int count = 0;
+int numChapter = 15;
 
 class ChapterList extends StatefulWidget {
   String idComic;
   final bool Emty;
 
-
-  ChapterList({Key? key, required this.idComic,required this.Emty}) : super(key: key);
+  ChapterList({Key? key, required this.idComic, required this.Emty})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-
-    if(Emty){
+    if (Emty) {
       return _EmtyChapterList();
-    }
-    else{
+    } else {
       return _ChapterList();
     }
-
   }
 }
 
-class _ChapterList extends State< ChapterList> {
-
+class _ChapterList extends State<ChapterList> {
   // final int chapterNumber;
   List<String>? names;
   List<String>? images;
-  List <Object?> ListImg=[];
+  List<Object?> ListImg = [];
   String? NameChapter;
   bool? Emty;
-  List <String> ChapterImg=[];
-  List <String> ChapterName=[];
-
+  List<String> ChapterImg = [];
+  List<String> ChapterName = [];
 
   @override
   Widget build(BuildContext context) {
-    List <String> NameList=[];
-    List <String> ImgList=[];
+    List<String> NameList = [];
+    List<String> ImgList = [];
 
-
-    _db.child('Comic/'+widget.idComic+'/Chapters').onValue.listen((event) {
-      List <Object?> listChapter=event.snapshot.value;
-        numChapter=listChapter.length;
+    _db
+        .child('Comic/' + widget.idComic + '/Chapters')
+        .onValue
+        .listen((event) {
+      List<Object?> listChapter = event.snapshot.value;
+      numChapter = listChapter.length;
     });
 
-    if(ChapterImg.length<numChapter){
-      for(var i=0;i<numChapter;i++){
-        String test="";
-        _db.child('Comic/'+widget.idComic+'/Chapters/'+i.toString()+'/Links/0').onValue.listen((event) {
-          if(event.snapshot.value!=null){
-            final String listChapter=event.snapshot.value;
-            test=listChapter;
+    if (ChapterImg.length < numChapter) {
+      for (var i = 0; i < numChapter; i++) {
+        String test = "";
+        _db
+            .child('Comic/' +
+            widget.idComic +
+            '/Chapters/' +
+            i.toString() +
+            '/Links/0')
+            .onValue
+            .listen((event) {
+          if (event.snapshot.value != null) {
+            final String listChapter = event.snapshot.value;
+            test = listChapter;
             ChapterImg.add(test);
           }
         });
       }
     }
 
-    if(ChapterName.length<numChapter){
-      for(var i=0;i<numChapter;i++){
-        _db.child('Comic/'+widget.idComic+'/Chapters/'+i.toString()+'/Name').onValue.listen((event) {
-          if(event.snapshot.value!=null){
-            final String listChapter=event.snapshot.value;
+    if (ChapterName.length < numChapter) {
+      for (var i = 0; i < numChapter; i++) {
+        _db
+            .child('Comic/' +
+            widget.idComic +
+            '/Chapters/' +
+            i.toString() +
+            '/Name')
+            .onValue
+            .listen((event) {
+          if (event.snapshot.value != null) {
+            final String listChapter = event.snapshot.value;
             NameList.add(listChapter);
-            ChapterName=NameList;
+            ChapterName = NameList;
           }
         });
       }
@@ -85,61 +91,93 @@ class _ChapterList extends State< ChapterList> {
 
 
 
-
     return SliverList(
       delegate: SliverChildBuilderDelegate(
-        (BuildContext context, int index) {
+            (BuildContext context, int index) {
           return GestureDetector(
-              onTap: () {
-                setState(() {
-                  _db.child('Comic/'+widget.idComic+'/Chapters/'+index.toString()+'/Links').onValue.listen((event) {
-                    List <Object?> listChapter=event.snapshot.value;
-                    ListImg=listChapter;
-                  });
+            onTap: () {
+              setState(() {
+                _db
+                    .child('Comic/' +
+                    widget.idComic +
+                    '/Chapters/' +
+                    index.toString() +
+                    '/Links')
+                    .onValue
+                    .listen((event) {
+                  List<Object?> listChapter = event.snapshot.value;
+                  ListImg = listChapter;
                 });
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ComicScreen(
-                            images: ListImg,NameChapter: ChapterName[index],
-                          )),
-                );
-              },
-              //products[index].imageURL
-              // if images and names list are null default mock is generated
-              child: chapterTile(index, ChapterImg[index], ChapterName[index]));
+              });
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      ComicScreen(
+                        images: ListImg,
+                        NameChapter: ChapterName[index],
+                      ),
+                ),
+              );
+            },
+            //products[index].imageURL
+            // if images and names list are null default mock is generated
+            child: chapterTile(index, ChapterImg[index], ChapterName[index]),
+          );
         },
-        childCount: numChapter, // 1000 list items
+        childCount: numChapter, // 1000 list items:
       ),
     );
   }
 
   chapterTile(int number, String? image, String? name) {
     return Container(
-      color: Colors.blueGrey,
-      height: 80,
+      // color: Colors.blueGrey,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: 0.2,
+          ),
+          top: BorderSide(
+            color: Colors.grey,
+            width: 0.2,
+          ),
+        ),
+      ),
+      height: 100,
       alignment: Alignment.center,
       child: Row(
         children: [
+          const Padding(padding: EdgeInsets.only(left: 25)),
           chapterImage(image ??
               "https://upload.wikimedia.org/wikipedia/vi/c/c7/Naruto_Volume_1_manga_cover.jpg"),
+          const Padding(padding: EdgeInsets.only(right: 20)),
           Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                name ?? 'The Chapter Name',
-                style: const TextStyle(fontSize: 15, color: Colors.white),
+              SizedBox(
+                width: 200,
+                child: Text(
+                  name ?? 'Chapter Name',
+                  style: const TextStyle(fontSize: 18, color: Colors.black),
+                  softWrap: true,
+                ),
               ),
               Text(
-                "Chapter $number",
-                style: const TextStyle(fontSize: 10),
+                "Ep. ${number + 1}",
+                style: const TextStyle(fontSize: 15, color: Colors.grey),
               )
             ],
           ),
           const Spacer(),
           Text(
             "#$number",
-            style: const TextStyle(fontSize: 15, color: Colors.white),
+            style: const TextStyle(fontSize: 17, color: Colors.black),
           ),
+          const Padding(padding: EdgeInsets.only(right: 25))
         ],
       ),
     );
@@ -147,7 +185,7 @@ class _ChapterList extends State< ChapterList> {
 
   chapterImage(String image) {
     return SizedBox(
-      width: 50,
+      width: 80,
       child: Image.network(
         image,
         fit: BoxFit.cover,
@@ -156,25 +194,20 @@ class _ChapterList extends State< ChapterList> {
   }
 }
 
-class _EmtyChapterList extends State<ChapterList>{
+class _EmtyChapterList extends State<ChapterList> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return SliverFixedExtentList(
       itemExtent: 50,
-      delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-          return Container(
-            alignment: Alignment.center,
-            child: Text('Comming soon',
-            style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-          );
-        },
-        childCount: 1
-      ),
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        return Container(
+          alignment: Alignment.center,
+          child: const Text(
+            'Comming soon',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+        );
+      }, childCount: 1),
     );
   }
-
-
 }
-
